cc2b683e6472d6c6a8b541f94a3f503eadd23bdb
Tushar <tushargoyal414@gmail.com>
2019-05-18 02:35:33 +0530

    build request & get response

--- app/src/main/java/com/example/github_followers/DashPage.java
+++ app/src/main/java/com/example/github_followers/DashPage.java
@@ -5,13 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class DashPage extends AppCompatActivity {
+    private static final String TAG ="DashPage" ;
     String user_url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +42,27 @@ public class DashPage extends AppCompatActivity {
         OkHttpClient client=new OkHttpClient();
         Request request=new Request.Builder()
                 .url(user_url).build();
-        try (Response response = client.newCall(request).execute())
-        {String value=response.body().string();
-        Log.d("DashPage",value);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException
+            {
+                try {
+                    String responsedata=response.body().string();
+                    JSONObject json = new JSONObject(responsedata);
+                    final String owner = json.getString("id");
+                    Log.d(TAG, "onResponse: "+owner);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
 
 
     }
