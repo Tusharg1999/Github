1d9f7991ba7a99784c35b99ac6f469e5aec1727a
Tushar <tushargoyal414@gmail.com>
2019-05-18 23:17:08 +0530

    getting data from api

--- app/src/main/java/com/example/github_followers/DashPage.java
+++ app/src/main/java/com/example/github_followers/DashPage.java
@@ -1,11 +1,14 @@
 package com.example.github_followers;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -18,8 +21,11 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class DashPage extends AppCompatActivity {
-    private static final String TAG ="DashPage" ;
+    private static final String TAG = "DashPage";
     String user_url;
+    Handler handler;
+    TextView userName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,15 +38,17 @@ public class DashPage extends AppCompatActivity {
     }
 
 
-
     private void init() {
-        Intent intent=getIntent();
-        String id=intent.getStringExtra("user_id");
-        user_url= "https://api.github.com/users";
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("user_id");
+        user_url = "https://api.github.com/users/" + id;
+        handler=new Handler(getApplicationContext().getMainLooper());
+        userName=findViewById(R.id.name_text);
     }
+
     private void getUserData() {
-        OkHttpClient client=new OkHttpClient();
-        Request request=new Request.Builder()
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
                 .url(user_url).build();
 
         client.newCall(request).enqueue(new Callback() {
@@ -50,15 +58,21 @@ public class DashPage extends AppCompatActivity {
             }
 
             @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException
-            {
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
                 try {
-                    String responsedata=response.body().string();
-                    JSONObject json = new JSONObject(responsedata);
-                    final String owner = json.getString("id");
-                    Log.d(TAG, "onResponse: "+owner);
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                    String responseData = response.body().string();
+                    Log.d(TAG, "onResponse: "+responseData);
+                    JSONObject json = new JSONObject(responseData);
+                    String owner = json.getString("name");
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            userName.setText(owner);
+                        }
+                    });
+
+                } catch (JSONException e1) {
+                    e1.printStackTrace();
                 }
 
             }
@@ -67,3 +81,5 @@ public class DashPage extends AppCompatActivity {
 
     }
 }
+
+
